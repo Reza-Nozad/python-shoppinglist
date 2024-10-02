8896ae82e3da5cfe0061860511ad2726472ea111
reza <reza.nozad@gmail.com>
2024-10-02 17:09:42 +0200

    hauptprogramm wurde erstellt

--- shoppinglist.py
+++ shoppinglist.py
@@ -21,6 +21,38 @@ def show_shoppinglist():
 show_shoppinglist()
 
 
+while True:
+    print("------------------------------------------------ Einkaufsliste --------------------------------------------------------")
+    print("1. Artikel zur Einkaufsliste hinzufügen")
+    print("2. Einkaufsliste anzeigen")
+    print("3. Programm beenden")
+    choice = input("welche nummer wählen Sie aus? (also 1, 2 oder 3)")
+    print("ihre Auswahl:", choice)
+    if choice==1:
+        add_item()
+    elif choice==2:
+        show_shoppinglist()
+    elif choice==3:
+        print("Programm wird beendet! Auf Wiedersehen")  
+        break
+    else:
+        print("Ungültige Auswahl. Bitte wähle 1, 2 oder 3")   
+          
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
 
   
 
