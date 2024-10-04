c1e3628a826a41eed99420a55e9e3ab8652e1abf
reza <reza.nozad@gmail.com>
2024-10-04 12:32:37 +0200

    die Aufgabe ist erledigt

--- shoppinglist.py
+++ shoppinglist.py
@@ -1,45 +1,57 @@
 shoppinglist=[]
+
 def add_item():
-    item = input("Bitte gib den Artikel ein, der zur Einkaufsliste hinzugefügt werden soll")
-    print ("Ihre Artikel :", item)
-    shoppinglist.append(item)
+    item = input("Bitte gib den Artikel ein, der zur Einkaufsliste hinzugefügt werden soll: ")
+    print ("Ihre Artikel : ", item)
+    if item:
+     shoppinglist.append(item)
     print("der Artikel wurde der Liste hinzugefügt")
-    print(shoppinglist)
-    
-add_item() 
+    #print(shoppinglist)
+      
+#add_item()
+
+
 
 
 def show_shoppinglist():
-   
+     
    if shoppinglist:
-        print ("your shoppinglist ", shoppinglist)
-
+       print("Deine Einkaufsliste:")
+       for item in shoppinglist:  
+        print(item) 
+     
    else:
-       
-        print("shoppinglist ist leer")
+        print("Deine Einkaufsliste ist leer")
 
-show_shoppinglist()
+#show_shoppinglist()
 
 
-while True:
+def main():
+
+ while True:
     print("------------------------------------------------ Einkaufsliste --------------------------------------------------------")
     print("1. Artikel zur Einkaufsliste hinzufügen")
     print("2. Einkaufsliste anzeigen")
     print("3. Programm beenden")
     choice = input("welche nummer wählen Sie aus? (also 1, 2 oder 3)")
     print("ihre Auswahl:", choice)
-    if choice==1:
+
+    if choice=="1":
         add_item()
-    elif choice==2:
+    elif choice=="2":
         show_shoppinglist()
-    elif choice==3:
+    elif choice=="3":
         print("Programm wird beendet! Auf Wiedersehen")  
         break
     else:
-        print("Ungültige Auswahl. Bitte wähle 1, 2 oder 3")   
+        print("Ungültige Auswahl. Bitte wähle 1, 2 oder 3") 
+          
+main()
 
-if __name__== "_main_":
-    main()
+#  if __name__== "_main_":
+#      main()
+   
+
 
   
 
